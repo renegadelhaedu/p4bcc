57b7a5e8aaccefaaf3515bb8fa53c162c7a21b2a
renegadelha <renegadelha@gmail.com>
2024-09-03 20:52:59 -0300

    aula0309

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import *
+import dao
 
 app = Flask(__name__)
+app.secret_key = 'testedechave'
 
 @app.route('/')
 def home():
@@ -8,9 +10,29 @@ def home():
 
 @app.route('/fazer_login', methods=['POST'])
 def fazer_login():
-    print(request.form.get('login'))
-    print(request.form.get('senha'))
-    return 'UHUUU CHEGOU A REQUISICAO'
+    login = request.form.get('login')
+    senha = request.form.get('senha')
+
+    if dao.verificarlogin(login, senha, dao.conectardb()):
+        session['login_user'] = login
+        return render_template('home.html', user=login)
+    else:
+        msg = 'Login ou senha incorretos'
+        return render_template('index.html', texto=msg)
+
+
+@app.route('/exibirPagComentario')
+def exibirPagComent():
+    return render_template('inserirmsg.html')
+
+@app.route('/comentario/inserir', methods=['POST'])
+def inserirmsgdatabase():
+    coment = request.form.get('mensagem')
+    print(coment + " - " + session['login_user'])
+    if dao.insert_comentario(session['login_user'], coment, dao.conectardb()):
+        return 'inseriu com sucesso!'
+    else:
+        return render_template(home.html, user=session['login_user'])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- dao/__init__.py
+++ dao/__init__.py
@@ -6,16 +6,34 @@ def conectardb():
         user='postgres',
         password='12345'
     )
-
     return con
 
 def verificarlogin(nome, senha, conexao):
 
     cur = conexao.cursor()
-    cur.execute(f"SELECT count(*) FROM usuarios WHERE login = '{nome}' AND senha = '{senha}'")
+    cur.execute(f"SELECT count(*) FROM usuario WHERE login = '{nome}' AND senha = '{senha}'")
     recset = cur.fetchall()
     conexao.close()
+
     if recset[0][0] == 1:
         return True
     else:
         return False
+
+def insert_comentario(login, comentario, conexao):
+
+    cur = conexao.cursor()
+    exito = False
+    try:
+        #mudar nome da tabela
+        sql = (f"UPDATE usuario SET comentario = '{comentario}' where login = '{login}'")
+        cur.execute(sql)
+    except psycopg2.IntegrityError:
+        conexao.rollback()
+        exito = False
+    else:
+        conexao.commit()
+        exito = True
+
+    conexao.close()
+    return exito
